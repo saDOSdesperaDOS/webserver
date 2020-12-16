e6e923edde685d93ef828559de4a61ab16930ef6
sadosdesperados <bms39rus@gmail.com>
2020-12-16 17:51:34 +0200

    Test for a task 2.3 from module 2 Authorization passed

--- L2.1 Authorization/src/main/java/servlets/SessionsServlet.java
+++ L2.1 Authorization/src/main/java/servlets/SessionsServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.logging.Logger;
 
 public class SessionsServlet extends HttpServlet {
@@ -39,33 +40,34 @@ public class SessionsServlet extends HttpServlet {
     public void doPost(HttpServletRequest request,
                        HttpServletResponse response) throws ServletException, IOException {
         String login = request.getParameter("login");
-        String pass = request.getParameter("password");
+        String password = request.getParameter("password");
 
-        if (login == null || pass == null) {
+        if (login == null || password == null) {
             response.setContentType("text/html;charset=utf-8");
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.setStatus(401);
             Logger.getGlobal().info("SessionServlet bad request " +
                     login + " " + response.getStatus());
+            response.getWriter().println("Unauthorized");
             return;
         }
 
         UserProfile profile = accountService.getUserByLogin(login);
-        if (profile == null || !profile.getPass().equals(pass)) {
+        if (profile == null || !profile.getPass().equals(password)) {
             response.setContentType("text/html;charset=utf-8");
-            response.setStatus(400);
-            Logger.getGlobal().info(String.valueOf(response.getStatus()));
+            response.setStatus(401);
             Logger.getGlobal().info("SessionServlet unauthorized " +
                     login + " " + response.getStatus());
+            response.getWriter().println("Unauthorized");
             return;
         }
 
         accountService.addSession(request.getSession().getId(), profile);
-        Gson gson = new Gson();
-        String json = gson.toJson(profile);
+        /*Gson gson = new Gson();
+        String json = gson.toJson(profile);*/
         response.setContentType("text/html;charset=utf-8");
-        response.getWriter().println(json);
         response.setStatus(HttpServletResponse.SC_OK);
         Logger.getGlobal().info("SessionServlet authorized " + login + " " + response.getStatus());
+        response.getWriter().println("Authorized: " + login);
     }
 
     //sign out
--- L2.1 Authorization/src/main/java/servlets/SignInServlet.java
+++ L2.1 Authorization/src/main/java/servlets/SignInServlet.java
@@ -15,16 +15,7 @@ public class SignInServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-        String login = req.getParameter("login");
         RequestDispatcher dispatcher = req.getRequestDispatcher("/api/v1/sessions");
         dispatcher.forward(req, resp);
-        if (resp.getStatus() == HttpServletResponse.SC_OK) {
-            PrintWriter out = resp.getWriter();
-            out.println("Authorized: " + login);
-        } else {
-            PrintWriter out = resp.getWriter();
-            out.println("Unauthorized");
-        }
-
     }
 }
